c64d2a958c928106be57eae7e853f6a6fe9f7301
siddhvasa <siddhvasa@gmail.com>
2026-06-03 12:09:01 +0000

    started rest api for the frontend

--- main.go
+++ main.go
@@ -1,63 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/websocket"
 	"siddh.com/compiler"
 	"siddh.com/graphter"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func webSocketHandler(w http.ResponseWriter, r *http.Request) {
-	// data := map[string]interface{}{
-	// 	"ni": 10,
-	// }
-	// jsonData, err := json.Marshal(data)
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Println(err)
-	}
-	defer conn.Close()
-
-	for {
-		_, msg, err := conn.ReadMessage()
-
-		if err != nil {
-			log.Println(err)
-			break
-		}
-
-		// var jsonMap map[string]interface{}
-		// json.Unmarshal(msg, &jsonMap)
-
-		// log.Printf("got message %s \n", msg)
-		fmt.Print(compiler.Parse((string(msg))))
-		// err = conn.WriteMessage(websocket.TextMessage, msg)
-		err = conn.WriteMessage(websocket.TextMessage, []byte("got it"))
-
-		if err != nil {
-			log.Println(err)
-			break
-		}
-
-	}
-}
-
-func K(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<html>hi</html>"))
-}
 
 func main() {
 	equation := compiler.Parse("((1x)^1 (+)^1 (1y)^1)^1 (+)^1 ((1x)^1 (+)^1 (1y)^1)^1 = (10)^1  (+)^1 (10x)^1")
